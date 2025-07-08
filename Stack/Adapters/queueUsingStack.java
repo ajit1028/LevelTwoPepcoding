625c253dd86249eebc4c9e6efe42b370eccff935
ajit <ajeets@1028.com>
2025-07-08 08:45:26 +0530

    queue using two stacks

---
+++ Stack/Adapters/queueUsingStack.java
@@ -0,0 +1,39 @@
+import java.util.LinkedList;
+
+public class queueUsingStack {
+
+    LinkedList<Integer> st = new LinkedList<>();
+    LinkedList<Integer> temp = new LinkedList<>();
+
+    public queueUsingStack() {
+
+    }
+
+    public void push(int x) {
+        st.addFirst(x);
+    }
+
+    public void swapData(LinkedList<Integer> st1, LinkedList<Integer> st2) {
+        while (st1.size() != 0) {
+            st2.addFirst(st1.removeFirst());
+        }
+    }
+
+    public int pop() {
+        swapData(st, temp);
+        int rv = temp.removeFirst();
+        swapData(temp, st);
+        return rv;
+    }
+
+    public int peek() {
+        swapData(st, temp);
+        int rv = temp.getFirst();
+        swapData(temp, st);
+        return rv;
+    }
+
+    public boolean empty() {
+        return st.size() == 0;
+    }
+}
